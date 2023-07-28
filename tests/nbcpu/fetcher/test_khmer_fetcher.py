0ea55b7fc5790c0ee65ae71459f051ad05880dbf
Young Joon Lee <entelecheia@hotmail.com>
2023-07-29 05:42:15 +0900

    test(nbcpu/fetcher): add new parameters to KhmerFetcher

--- tests/nbcpu/fetcher/test_khmer_fetcher.py
+++ tests/nbcpu/fetcher/test_khmer_fetcher.py
@@ -8,7 +8,11 @@ def test_save_hyfi_config():
 def test_khmer_fetcher():
     khmer = KhmerFetcher(
         search_keywords=["NBC"],
+        max_num_pages=1,
+        max_num_articles=5,
         verbose=True,
+        output_dir="workspace/test/khmer",
+        overwrite_existing=True,
     )
     assert khmer.search_url == "https://www.khmertimeskh.com/page/{page}/?s={keyword}"
     khmer.fetch_links()
